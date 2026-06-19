d5adde98a22db739df0aa6fd178c47c707ff5bb9
kabilramar <kabilramar@gmail.com>
2026-06-19 12:48:25 +0530

    Add files via upload

---
+++ AB.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class AB {
+    public static void main(String[] args) 
+    {
+     
+      String b;
+      Scanner s=new Scanner(System.in);
+      b=s.nextLine();
+      char a[]=b.toCharArray();
+      int n=a.length;
+   System.out.println(b.trim());
+       
+}
+}
